4f68b11acf4b8897e1c2d49a58e2e6bae7a02be7
Ryan-Renjith <ryanrenjith19@gmail.com>
2024-06-22 01:03:15 +0530

    near completed version

--- index.js
+++ index.js
@@ -1,26 +1,68 @@
-require('dotenv').config();
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline-sync');
+const dotenv = require('dotenv');
+
+dotenv.config(); // Load variables from .env into process.env
+const envPath = path.join(__dirname, '.env');
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { argv } = require('process');
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+async function getApiKey() {
+    try {
+        const apiKey = process.env.GEMINI_API_KEY;
+        if (!apiKey) {
+            console.log('GEMINI_API_KEY not found');
+            const newApiKey = readline.question('Enter your GEMINI_API_KEY: ');
+            // Update .env file
+            fs.writeFileSync(envPath, `GEMINI_API_KEY=${newApiKey}\n`);
+            console.log('gprompt updated with GEMINI_API_KEY.');
+            return newApiKey;
+        }
+        return apiKey;
+    } catch (err) {
+        console.error('Error reading .env file:', err);
+        return null;
+    }
+}
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-
-
-async function run(prompt) {
+async function run(prompt, model) {
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
     console.log(text);
   }
-  
 
-const args = process.argv.slice(2);
-let prompt;
+async function main() {
+    const apiKey = await getApiKey();
 
-if (args.length === 0) {
-    console.log('Please provide the prompt');
-} else {
-    prompt = args.join(' ');
-    run(prompt);
+    if (!apiKey) {
+        console.error('API key not found or could not be retrieved.');
+        return;
+    }
+    
+    const genAI = new GoogleGenerativeAI(apiKey);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+    const args = process.argv.slice(2);
+    let prompt;
+
+    if (args.length === 0) {
+        console.log('Please provide the prompt');
+    } else {
+        if(args[0] === 'deleteKey') {
+            delete process.env.GEMINI_API_KEY;
+            fs.writeFileSync(envPath, `GEMINI_API_KEY=`);
+            console.log('API key deleted');
+            return;
+        }
+
+        prompt = args.join(' ');
+        run(prompt, model);
+    }
 }
+
+main();
